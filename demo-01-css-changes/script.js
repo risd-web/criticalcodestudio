5f41fec0fc76a9a95e31e8cc7b0ebba08fb2001a
Marie Otsuka <motsuka.gd@gmail.com>
2020-09-24 14:30:43 -0400

    demo file updates

--- demo-01-css-changes/script.js
+++ demo-01-css-changes/script.js
@@ -3,19 +3,21 @@ $(document).ready(function(){
 	// Get existing values
 	$('#inspect').click(function(){
 		$element = $('#sample');
+		//reset sample
+		$('#sample').removeAttr('style')
 		
 		// store calculated css 
 		let size = $element.css('font-size');
 		let leading = $element.css('line-height');
 		let tracking = $element.css('letter-spacing');
 		let slant = $element.css('font-style');
-		console.log(slant);
 
 		// display these values
 		$('#applied-size').text(size);
 		$('#applied-leading').text(leading);
 		$('#applied-tracking').text(tracking);
 
+		// toggle data attribute on roman
 		if(slant == 'italic'){
 			$('#options-slant').attr('data-slant', 'italic');
 		}else{
@@ -25,45 +27,53 @@ $(document).ready(function(){
 		// set these values on the sliders
 		$('#slider-size').val( size.replace('px','') );
 
-
-
 	});
 
 	// input sliders
 
 	$(document).on('input', '#slider-size', function() {
-		value = $(this).val(); //get slider input value
-		console.log(value);
+		let sizevalue = $(this).val(); //get slider input value
 
 		//adjust CSS of sample
-		$('#sample').css('font-size', value + 'px');
+		$('#sample').css('font-size', sizevalue + 'px');
 		
 		//indicate value
-		$('#applied-size').text(value + 'px');
+		$('#applied-size').text(sizevalue + 'px');
 	});
 
 	$(document).on('input', '#slider-leading', function() {
-		value = $(this).val(); //get slider input value
-		console.log(value);
+		let leadingvalue = $(this).val(); //get slider input value
 
 		//adjust CSS of sample
-		$('#sample').css('line-height', value);
+		$('#sample').css('line-height', leadingvalue);
 		
 		//indicate value
-		$('#applied-leading').text(value);
+		$('#applied-leading').text(leadingvalue);
 	});
 
 	$(document).on('input', '#slider-tracking', function() {
-		value = $(this).val(); //get slider input value
-		console.log(value);
+		let trackingvalue = $(this).val(); //get slider input value
 
 		//adjust CSS of sample
-		$('#sample').css('letter-spacing', value + 'px');
+		$('#sample').css('letter-spacing', trackingvalue + 'px');
 		
 		//indicate value
-		$('#applied-tracking').text(value + 'px');
+		$('#applied-tracking').text(trackingvalue + 'px');
 	});
 
+
+	// italic toggle
+
+	$('.slant-option').click(function(){
+		let slantvalue = $(this).attr('id');
+		$('#options-slant').attr('data-slant', slantvalue);
+
+		if(slantvalue == 'italic'){
+			$('#sample').css('font-style', 'italic');
+		}else{
+			$('#sample').css('font-style', 'normal');
+		}
+	});
 
 
 });
